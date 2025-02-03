5e70bb6e0ab1efc5d50185e71499c259bf6d95d1
Didier Nsengiyumva <nsedidier@gmail.com>
2025-02-03 10:21:09 +0200

    Update app.js 1021

--- app.js
+++ app.js
@@ -560,7 +560,7 @@ app.post("/api/get-menu", async (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
     await initializeFlow();
 });
